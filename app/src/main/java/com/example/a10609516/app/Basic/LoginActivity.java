3662747901c28824cd002bbe5673a4317866d1d1
MartinLin08 <martin91218@gamil.com>
2018-06-13 15:59:39 +0800

    1.新增線上更新APP功能 2.派工回報增加收款方式的選擇

--- app/src/main/java/com/example/a10609516/app/Basic/LoginActivity.java
+++ app/src/main/java/com/example/a10609516/app/Basic/LoginActivity.java
@@ -309,7 +309,6 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "更新錯誤!", Toast.LENGTH_LONG).show();
                 }
             });
-
         }
     }
 
